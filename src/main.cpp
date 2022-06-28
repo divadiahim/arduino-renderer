05a1ec3115fc898e631771850a2281b2c2266f77
divadiahim <53019161+divadiahim@users.noreply.github.com>
2022-06-28 21:24:35 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ struct Vector2
 {
     uint16_t x, y;
 };
-
+uint8_t framebuf[6][84];
 void lcd_write(byte data)
 {
 
@@ -30,13 +30,24 @@ void lcd_write(byte data)
 }
 void clear_ram()
 {
+  
     for (uint16_t i = 0; i <= 503; i++)
     {
         // shiftOut(LCD_DIN, LCD_CLK, 1, 0x00);
         lcd_write(0x00);
     }
 }
-void drawpixel(uint8_t x, uint8_t y)
+void clear()
+{
+      for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < 84; j++)
+        {
+            framebuf[i][j] = 0;
+        }
+    }
+}
+void drawpixel(uint8_t x, uint8_t y) // this is using the command mode | chnges the memeory adress
 {
     PORTD = PORTD & ~(1 << LCD_DC);
     byte y_ = y / 8;
@@ -48,17 +59,21 @@ void drawpixel(uint8_t x, uint8_t y)
     PORTD = PORTD | (1 << LCD_DC);
     lcd_write(0x01 << y_pixel);
 }
-void draw_line(Vector2 *poz)
+void put_pixel(uint8_t x, uint8_t y)
 {
-    uint8_t x1 = 0, y1 = 0;
-    uint8_t dx = poz->x - x1;
-    uint8_t dy = poz->y - y1;
-    float m = dy / dx;
-    float b = y1 - (m * x1);
-    int j = 0;
-    for (size_t i = 0; i < dx; i++)
+    byte y_ = y / 8;
+    byte y_pixel = y - y_ * 8;
+    framebuf[y_][x] |= (0x01 << y_pixel);
+  
+}
+void update()
+{
+    for (int i = 0; i < 6; i++)
     {
-        drawpixel((uint8_t)x1 + i, (uint8_t)round(m * (x1 + i) + b));
+        for (int j = 0; j < 84; j++)
+        {
+            lcd_write(framebuf[i][j]);
+        }
     }
 }
 void plot_line(int x0, int y0, int x1, int y1)
@@ -67,9 +82,9 @@ void plot_line(int x0, int y0, int x1, int y1)
     int dy = -abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
     int err = dx + dy, e2; /* error value e_xy */
 
-    for (size_t i = 0; i < sqrt(x1*x1+y1*y1); i++)
+    for (;;)
     { /* loop */
-        drawpixel(x0, y0);
+        put_pixel(x0, y0);
         if (x0 == x1 && y0 == y1)
             break;
         e2 = 2 * err;
@@ -84,6 +99,7 @@ void plot_line(int x0, int y0, int x1, int y1)
             y0 += sy;
         } /* e_xy+e_y < 0 */
     }
+    update();
 }
 
 void init_lcd()
@@ -107,33 +123,36 @@ int main(void)
 {
     init();
     init_lcd();
-    // drawpixel(3, 1);
-    Vector2 line{37, 42};
-    // draw_line(&line);
+    Vector2 line{3, 15};
     uint8_t i = 0;
     bool ok = 0;
     while (true)
     {
-        if (i < 65 && ok == 0)
+        if (i < 83 && ok == 0)
         {
             i++;
         }
-        if (i == 65 && ok == 0)
+        if (i == 83 && ok == 0)
         {
             ok = 1;
         }
-        if (i <= 65 && ok == 1)
+        if (i <= 83 && ok == 1)
         {
             i--;
         }
-        if (i == 0 && ok == 1)
+        if (i == 1 && ok == 1)
         {
             ok = 0;
         }
 
-        plot_line(0, 0, i, 30);
-        delay(20);
-        clear_ram();
+        //plot_line(i, 2, i, 30);;
+       // put_pixel(5,i);
+        // bhm_line(i,0,i,30);
+        
+        plot_line(1,1,i,30);
+        delay(50);
+        clear();
+        // clear_ram();
     }
 
     // lcd_write(0x7F);
